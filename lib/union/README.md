e55db409153b84611c60627bf05b3c67a683dd0a
Milan Felix Šulc <rkfelix@gmail.com>
2021-12-30 22:08:28 +0100

    Union: reworked

--- lib/union/README.md
+++ lib/union/README.md
@@ -4,32 +4,6 @@ Install all packages together and see if they are dependency compatible.
 
 ## Usage
 
-Install it via `composer install` or update via `composer update`.
-
-## Todo
-
-List of missing packages.
-
-```json
-	"contributte/qa": "*",
-	"contributte/mobilni-platby": "*",
-	"contributte/facebook": "*",
-	"contributte/nextras-orm-events": "*",
-	"contributte/latte-parsedown-extra": "*",
-	"contributte/replacus": "*",
-	"contributte/codeception": "*",
-	"contributte/database": "*",
-	"contributte/czech-post": "*",
-	"contributte/anabelle": "*",
-	"contributte/forms-multiplier": "*",
-
-	"apitte/events": "*",
-	"apitte/presenter": "*",
-
-	"nettrine/extensions-atlantic18": "*",
-	"nettrine/extensions-beberlei": "*",
-	"nettrine/extensions-oroinc": "*",
-	"nettrine/extensions-knplabs": "*"
-
-	"nettrine/odm": "*",
-```
+- `make install` - install composer dependencies
+- `make update` - update composer dependencies
+- `make build` - refresh list of our dependencies from packagist
---
+++ lib/union/build.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types = 1);
+
+$sources = [
+	'https://packagist.org/packages/list.json?vendor=apitte&fields[]=abandoned',
+	'https://packagist.org/packages/list.json?vendor=contributte&fields[]=abandoned',
+	'https://packagist.org/packages/list.json?vendor=nettrine&fields[]=abandoned',
+	'https://packagist.org/packages/list.json?vendor=ninjify&fields[]=abandoned',
+];
+
+$skipped = [
+	// skeletons
+	"contributte/apitte-skeleton",
+	"contributte/console-skeleton",
+	"contributte/datagrid-skeleton",
+	"contributte/doctrine-skeleton",
+	"contributte/gui-skeleton",
+	"contributte/micro-skeleton",
+	"contributte/starter-skeleton",
+	"contributte/webapp-skeleton",
+	"contributte/webpack-skeleton",
+	// @todo (facebook fork)
+	"contributte/facebook",
+	// @todo (idk)
+	"contributte/apitte",
+	// @todo upgrade nette
+	"contributte/mobilni-platby",
+	"contributte/firewall",
+	'contributte/console-extra',
+	'contributte/scheduler',
+	'contributte/social',
+];
+
+$dependencies = ['php' => '>=8.0'];
+
+foreach ($sources as $source) {
+	$data = (array) json_decode(file_get_contents($source), true);
+
+	foreach ($data['packages'] as $package => $meta) {
+		// Skip abandoned
+		if ($meta['abandoned'] !== false) continue;
+
+		// Skip skipped
+		if (in_array($package, $skipped)) continue;
+
+		$dependencies[$package] = '*';
+	}
+}
+
+$composer = [
+	'require' => $dependencies,
+	"minimum-stability" => "dev",
+	"prefer-stable" => true,
+	"config" => [
+		"sort-packages" => true,
+		"allow-plugins" => [
+			"composer/package-versions-deprecated" => true,
+			"dealerdirect/phpcodesniffer-composer-installer" => true,
+		],
+	],
+];
+
+file_put_contents('composer.json', json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
